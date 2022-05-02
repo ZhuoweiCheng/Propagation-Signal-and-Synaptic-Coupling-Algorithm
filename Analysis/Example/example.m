79af10c9909f648cdca1187f6f9cc3912dd518c0
Destinee Cheng <53916384+ZhuoweiCheng@users.noreply.github.com>
2022-05-02 00:50:47 -0700

    Rename Analysis/example.m to Analysis/Example/example.m

---
+++ Analysis/Example/example.m
@@ -0,0 +1,1 @@
+[ListofPropagation, Time_all] = automated_detection_propagation(spike_times, 1, 180, 0.5, 50, 50);
--- Analysis/example.m
+++
@@ -1,1 +0,0 @@
-[ListofPropagation, Time_all] = automated_detection_propagation(spike_times, 1, 180, 0.5, 50, 50);

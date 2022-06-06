a14c6a783a4883824a43887ad7ed12cadc333e16
Destinee Cheng <53916384+ZhuoweiCheng@users.noreply.github.com>
2022-06-06 00:02:03 -0700

    Add files via upload

--- Analysis/Example/example.m
+++ Analysis/Example/example.m
@@ -1,1 +1,14 @@
-[current_propagation, Time_all] = automated_detection_propagation(spike_times, 1, 180, [], 0.5, 50, 50);
+%%
+%spike_times is the spike times from 120 electrodes. Each cell contains the
+%spike time from one electrode. The time unit is ms. This recording is 180s
+%in total and we used 1hz as the thresholding for the spiking frequency of
+%the reference electrode. Put [] for thres_number_spikes.
+[ListofPropagation, Time_all] = automated_detection_propagation(spike_times, 1, 180, [], 0.5, 50, 50);
+
+%%
+%Instead of thresholding for spiking frequency of the reference electrode,
+%you can also use an absolute number to threshold the total number of
+%spikes needed on each electrode. Put [] for thres_freq and 
+%seconds_recording and the number you want to use for thres_number_spikes.
+%Here we put 180.
+[ListofPropagation, Time_all] = automated_detection_propagation(spike_times, [], [], 180, 0.5, 50, 50);
